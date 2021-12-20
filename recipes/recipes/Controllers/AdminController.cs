db945be8e0955392d3b796284312e9ba232ec89c
umutsonmez <63147784+umutsonmez@users.noreply.github.com>
2021-12-20 15:12:36 +0300

    Admin Paneli
    Admin Paneli oluşturulmaya başlandı

---
+++ recipes/recipes/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using recipes.Models;
+using recipes.Data;
+
+namespace recipes.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AdminController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var values = _db.Recipe.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult NewRecipe()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewRecipe(Recipe r)
+        {
+            _db.Recipe.Add(r);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
